9731c3d014f0d9edf4208707faca4cabf711d00a
Tim O'Farrell <tofarr@everyonesocial.com>
2019-12-12 14:13:24 -0700

    Attempting to use the docs directory

--- dist.js
+++ dist.js
@@ -1,10 +1,10 @@
 const { exec } = require('child_process');
 
 console.log('CREATING DISTRIBUTION')
-console.log('This will refresh the contents of the /dist directory...')
+console.log('This will refresh the contents of the /docs directory...')
 console.log('This should be pushed to github where it will be served from https://tofarr.github.io/ash/dist')
 
-exec('rm -rf /dist', (err, stdout, stderr) => {
+exec('rm -rf /docs', (err, stdout, stderr) => {
   if (err) {
     // node couldn't execute the command
     return;
@@ -16,4 +16,4 @@ exec('rm -rf /dist', (err, stdout, stderr) => {
 })
 
 
-exec('cp -r build dist')
+exec('cp -r build docs')
